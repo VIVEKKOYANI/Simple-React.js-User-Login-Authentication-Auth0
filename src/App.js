0d9ffb865a712b9b67e189866f33f2ed1014d6de
vivek <vivek.koyani@gmail.com>
2020-09-28 17:49:01 +0530

    add react setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+     <div>
+       <h1>Hello</h1>
+     </div>
+    );
+  }
+}
+export default App;
